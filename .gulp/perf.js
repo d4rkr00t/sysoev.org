61af5f74c43aaa5adc8c963df6a1f8e258738e08
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-05-17 22:49:38 +0300

    Build critical css once

--- .gulp/perf.js
+++ .gulp/perf.js
@@ -1,16 +1,20 @@
 module.exports = function (gulp, paths) {
     var critical = require('critical'),
-        fs = require('fs');
+        fs = require('fs'),
+        width = 320,
+        height = 568;
 
     gulp.task('perf:critical', function () {
         critical.generate({
             base: './',
             src: 'public/index.html',
             css: [paths.prod.dest.styles + '/style.css'],
-            width: 320,
-            height: 480,
+            width: width,
+            height: height,
             minify: true
         }, function (err, output) {
+            if (err) return;
+
             fs.writeFile(
                 'themes/sysoev.org/layout/_partial/critical-css/main.ejs', '<style>' + output + '</style>'
             );
@@ -18,12 +22,14 @@ module.exports = function (gulp, paths) {
 
         critical.generate({
             base: './',
-            src: 'public/dega-event-delegation/index.html',
+            src: 'public/es6-arrow-functions/index.html',
             css: [paths.prod.dest.styles + '/style.css'],
-            width: 320,
-            height: 480,
+            width: width,
+            height: height,
             minify: true
         }, function (err, output) {
+            if (err) return;
+
             fs.writeFile(
                 'themes/sysoev.org/layout/_partial/critical-css/post.ejs', '<style>' + output + '</style>'
             );
@@ -33,10 +39,12 @@ module.exports = function (gulp, paths) {
             base: './',
             src: 'public/tags/javascript/index.html',
             css: [paths.prod.dest.styles + '/style.css'],
-            width: 320,
-            height: 480,
+            width: width,
+            height: height,
             minify: true
         }, function (err, output) {
+            if (err) return;
+
             fs.writeFile(
                 'themes/sysoev.org/layout/_partial/critical-css/tag.ejs', '<style>' + output + '</style>'
             );
